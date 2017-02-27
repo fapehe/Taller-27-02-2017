e2e9370448e86077c5e22eaf03a3605535558ede
Estudiantes <Estudiantes@LABING70203>
2017-02-27 17:29:26 -0500

    Versión 1.0

--- Prueba/src/prueba/Calculador.java
+++ Prueba/src/prueba/Calculador.java
@@ -11,8 +11,26 @@ package prueba;
  */
 public class Calculador {
     
-    float numero1;
-    float numero2;
+    private float numero1;
+    private float numero2;
+
+    public float getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(float numero1) {
+        this.numero1 = numero1;
+    }
+
+    public float getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(float numero2) {
+        this.numero2 = numero2;
+    }
+    
+    
     
     
     float sumar(){
--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -17,7 +17,33 @@ public class Prueba {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //Esta es mi primera contribuci{on al repositorio.
+        Calculador miCalculador=new Calculador();
+        Consola miConsola=new Consola();
+        int respuesta=miConsola.menuPrincipal();
+        float resultado;
+        
+        
+        miCalculador.setNumero1(miConsola.ingresarDato());
+        miCalculador.setNumero2(miConsola.ingresarDato()); 
+        
+        switch(respuesta){
+            
+            case 1:
+                miConsola.imprimir(miCalculador.sumar());
+                break;
+                
+            case 2:
+                miConsola.imprimir(miCalculador.restar());                
+                break;
+                
+            
+        }
+        
+        
+        
+        
+        
+        
     }
     
 }
